c56a56f1bfc5ac0304fa26f6c4d0f48ad9e02324
Carlos Martinez <martinezcj2@gmail.com>
2019-06-13 22:37:26 -0400

    Add snackbar and style color select

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import SnackBar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import './navbar.css';
 
 class Navbar extends Component {
   state = {
-    format: 'hex'
+    format: 'hex',
+    isOpen: false
   }
 
   handleFormatChange = async (e) => {
-    await this.setState({ format: e.target.value});
+    await this.setState({ format: e.target.value, isOpen: true});
     await this.props.handleFormatChange(this.state.format);
+  }
+
+  handleButton = () => {
+    this.state.isOpen && this.setState({ isOpen: false });
   }
   
   render() {
     const { colorLevel, handleLevelChange } = this.props;
-    const { format } = this.state;
+    const { format, isOpen } = this.state;
 
     return (
       <nav className="navbar">
@@ -44,6 +52,24 @@ class Navbar extends Component {
             <MenuItem value="hsl">HSL - hsl(0, 0%, 100%)</MenuItem>
           </Select>
         </div>
+        <SnackBar 
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}}
+          open={isOpen}
+          autoHideDuration={3000}
+          message={<span id='message-id'>Format changed to {format.toUpperCase()}!</span>}
+          ContentProps={{ 'aria-describedby': 'message-id'}}
+          onClose={this.handleButton}
+          action={[
+            <IconButton 
+              key="close" 
+              aria-label='close'
+              color="inherit"
+              onClick={this.handleButton}
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </nav>
     );
   }
