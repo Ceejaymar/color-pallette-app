17f98972614eb2ef17a58e59c0cee4ba9fd5be39
Carlos Martinez <martinezcj2@gmail.com>
2019-07-06 21:35:12 -0400

    Start styling minipalette

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -2,18 +2,42 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 
 const styles = {
-  main: {
-    backgroundColor: 'purple',
-    border: '3px solid teal'
+  root: {
+    // backgroundColor: 'purple',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'hover'
+    }
+  },
+  colorList: {
+    backgroundColor: 'white'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emojiIcon: {
+    marginRight: '0.5rem',
+    fontSize: '1.5rem'
   }
 }
 
-const MiniPalette = ({ classes }) => {
-  const { main } = classes;
-
+const MiniPalette = ({ classes, colors, id, emoji, paletteName }) => {
+  const { root, colorList, title, emojiIcon } = classes;
+  
   return (
-    <div className={main}>
-      <h1>Mini palette</h1>
+    <div className={root}>
+      <div className={colorList}>
+      <h5 className={title}>{paletteName}<span className={emojiIcon}>{emoji}</span></h5>
+      </div>
     </div>
   );
 };
--- src/containers/PaletteBoard.js
+++ src/containers/PaletteBoard.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
 import MiniPalette from '../components/MiniPalette';
+
+const styles = {
+  root: {
+    backgroundColor: 'blue'
+  },
+  container: {
+
+  },
+  nav: {
+
+  },
+  palette: {
+
+  }
+}
 
 class PaletteBoard extends Component {
   render() {
-    const { palettes } = this.props;
+    const { palettes, classes: { root, container, nav, palette } } = this.props;
 
     return (
-      <div>
+      <div className={root}>
+      <div className={container}>
+        <nav className={nav}>
+          <h1>Color picker</h1>
+        </nav>
+        <div className={palette}></div>
+      </div>
         <h1>Color Picker</h1>
 
         { palettes.map(palette => (
-          <Link to={`/palette/${palette.id}`}><MiniPalette /></Link>
+          <MiniPalette key={palette.id} {...palette}/>
         ))}
       </div>
     );
   }
 }
 
-export default PaletteBoard;
+export default withStyles(styles)(PaletteBoard);
