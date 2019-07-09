67730cb7ddb59a4a3aefa5cdd57cc74b1b282431
Carlos Martinez <martinezcj2@gmail.com>
2019-07-09 03:04:09 -0400

    Adjust text color by luminosity

--- src/containers/ColorBox.js
+++ src/containers/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import chroma from 'chroma-js';
 import './colorBox.css';
 
 class ColorBox extends Component {
@@ -20,8 +21,8 @@ class ColorBox extends Component {
   
   render() {
     const { background, name, paletteId, colorId, showLink } = this.props;
-    const { copyOverlay } = this.state;
-    const show = copyOverlay ? 'show' : '';
+    const show = this.state.copyOverlay ? 'show' : '';
+    const textColor = chroma(background).luminance() <= 0.06 ? 'color-box__light-text' : 'color-box__dark-text';
 
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -29,18 +30,18 @@ class ColorBox extends Component {
           <div className={`color-box__copy-overlay ${show}`} style={{ background }} />
           <div className={`color-box__copy-msg ${show}`}>
             <h2>copied!</h2>
-            <p>{this.props.background}</p>
+            <p className={`${textColor}`}>{this.props.background}</p>
           </div>
           <div className='color-box__copy-box'>
             <div className='color-box__content'>
-              <span className='color-box__name'>{name}</span>
+              <span className={`color-box__name ${textColor}`}>{name}</span>
             </div>
-            <button className='color-box__copy-btn'>Copy</button>
+            <button className={`color-box__copy-btn ${textColor}`}>Copy</button>
           </div>
           {
             showLink && (
               <Link to={`/palette/${paletteId}/${colorId}`} onClick={e => e.stopPropagation()}>
-                <span className='color-box__see-more'>More colors</span>
+                <span className={`color-box__see-more ${textColor}`}>More colors</span>
               </Link>
             )
           }
